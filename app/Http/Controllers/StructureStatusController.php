dbea6db0e400a09d1beb3dd11f49e82505a6147d
marvin255 <postbox433@gmail.com>
2019-10-14 17:52:36 +0200

    create resources controllers, fix code style

---
+++ app/Http/Controllers/StructureStatusController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Response;
+
+class StructureStatusController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return Response
+     */
+    public function index()
+    {
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param int $id
+     *
+     * @return Response
+     */
+    public function show($id)
+    {
+    }
+}
